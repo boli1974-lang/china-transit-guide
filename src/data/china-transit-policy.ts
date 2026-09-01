a88af52dd2d1100bd8757ea58f77512d279e499c
Bolei Shen <boli1974@gmail.com>
2026-09-01 02:23:42 -0700

    Add initial transit port data

--- src/data/china-transit-policy.ts
+++ src/data/china-transit-policy.ts
@@ -7,4 +7,47 @@
   source: string;
 }
 
-export const chinaTransitPorts: ChinaTransitPort[] = [];
+export const chinaTransitPorts: ChinaTransitPort[] = [
+  {
+    region: 'Beijing',
+    portName: 'Beijing Capital International Airport',
+    portType: 'airport',
+    permittedStayArea: 'Beijing Municipality',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+  {
+    region: 'Beijing',
+    portName: 'Beijing Daxing International Airport',
+    portType: 'airport',
+    permittedStayArea: 'Beijing Municipality',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+  {
+    region: 'Tianjin',
+    portName: 'Tianjin Binhai International Airport',
+    portType: 'airport',
+    permittedStayArea: 'Tianjin Municipality',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+  {
+    region: 'Tianjin',
+    portName: 'Tianjin Port (Passenger)',
+    portType: 'seaport',
+    permittedStayArea: 'Tianjin Municipality',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+  {
+    region: 'Hebei',
+    portName: 'Shijiazhuang Zhengding International Airport',
+    portType: 'airport',
+    permittedStayArea: 'Hebei Province',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+  {
+    region: 'Hebei',
+    portName: 'Qinhuangdao Port (Passenger)',
+    portType: 'seaport',
+    permittedStayArea: 'Hebei Province',
+    source: 'NIA 240-hour visa-free transit port list',
+  },
+];
